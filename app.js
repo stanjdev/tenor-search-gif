6f0f23283c2614515cb2be80eb7a63aa35ca69a6
Stanley Jeong <stanley.jeong@shopify.com>
2022-03-22 11:02:14 -0400

    first route built

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+// Middleware
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello Squirrel');
+});
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Gif search listening on port localhost:3000');
+})
