99f78a8decdbdec09ed06f8b4222535d5dfba491
Stanley Jeong <stanley.jeong@shopify.com>
2022-03-22 12:15:58 -0400

    styled page

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const Tenor = require('tenorjs').client({
+  "Key": "M0CEO4N4KXT1",
+  "Filter": "high",
+  "Locale": "en_US",
+})
 
 const app = express();
+app.use(express.static('public'));
 
 // Middleware
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+// app.set('views', './views');
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello Squirrel');
+  term = ""
+  if (req.query.term) {
+    term = req.query.term
+  }
+  // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+  Tenor.Search.Query(term, "10")
+    .then((response) => {
+      const gifs = response;
+      res.render('home', { gifs })
+    }).catch(console.error);
 });
+
+app.get('/greetings/:name', (req, res) => {
+  const name = req.params.name;
+  res.render('greetings', { name });
+})
 
 // Start Server
 
